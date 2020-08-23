09e3fcb79f832055847f6d4cdf1b931b669eea94
SAAD ARROUB <30563109+saadarroub@users.noreply.github.com>
2020-08-23 22:33:24 +0100

    msg

--- private/classes/bicycle.class.php
+++ private/classes/bicycle.class.php
@@ -34,6 +34,17 @@ class Bicycle
     return self::find_by_sql($sql);
   }
 
+  static public function find_by_id($id){
+    $sql = "SELECT * FROM bicycles ";
+    $sql .= "WHERE id = '" . self::$database->escape_string($id) . "'";
+    $obj_array = self::find_by_sql($sql);
+    if(!empty($obj_array)){
+      return array_shift($obj_array);
+    }else{
+      return false;
+    }
+  }
+
   static protected function instantiate($record)
   {
     $object = new self;
@@ -48,6 +59,7 @@ class Bicycle
   }
   // ----- END OF ACTIVE RECORD CODE ------
 
+  public $id;
   public $brand;
   public $model;
   public $year;
@@ -91,6 +103,10 @@ class Bicycle
     //     $this->$k = $v;
     //   }
     // }
+  }
+
+  public function name() {
+    return "{$this->brand} {$this->model} {$this->year}";
   }
 
   public function weight_kg()
