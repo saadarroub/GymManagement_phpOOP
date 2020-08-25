30b0d0c91961c5e02d084cd2a793e175a5586f6e
SAAD ARROUB <30563109+saadarroub@users.noreply.github.com>
2020-08-25 18:52:32 +0100

    msg

--- private/classes/bicycle.class.php
+++ private/classes/bicycle.class.php
@@ -6,6 +6,7 @@ class Bicycle
   // ----- START OF ACTIVE RECORD CODE ------
   static public $database;
   static protected $db_columns = ['id', 'brand', 'model', 'year', 'category', 'color', 'gender', 'price', 'weight_kg', 'condition_id', 'description'];
+  public $errors = [];
 
   static public function set_database($database)
   {
@@ -61,8 +62,22 @@ class Bicycle
     }
   }
 
+  protected function validate(){
+    $this->errors = [];
+    if(is_blank($this->brand)){
+      $this->errors[] = 'Brand cannot be blank.';
+    }
+    if(is_blank($this->model)){
+      $this->errors[] = 'Model cannot be blank.';
+    }
+    return $this->errors;
+  }
+
   protected function create()
   {
+    $this->validate();
+    if(!empty($this->errors)){ return false; }
+
     $attributes = $this->sanitized_attributes();
     $sql = "INSERT INTO bicycles (";
     $sql .= join(', ', array_keys($attributes));
@@ -78,6 +93,10 @@ class Bicycle
 
   protected function update()
   {
+
+    $this->validate();
+    if(!empty($this->errors)){ return false; }
+
     $attributes = $this->sanitized_attributes();
     $attribute_pairs = [];
     foreach ($attributes as $key => $value) {
