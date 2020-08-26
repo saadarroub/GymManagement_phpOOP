f8679eea40110de16b28789b8e5a6e84fb4dffbb
SAAD ARROUB <30563109+saadarroub@users.noreply.github.com>
2020-08-26 20:14:32 +0100

    msg

--- private/classes/admin.class.php
+++ private/classes/admin.class.php
@@ -1,6 +1,26 @@
 <?php
 
+class Admin extends DatabaseObject {
+    static protected $table_name = 'admins';
+    static protected $db_columns = ['first_name', 'last_name', 'email', 'username', 'hashed_password'];
 
+    public $id;
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $username;
+    public $hashed_password;
+
+    public function __construct($args = [])
+    {
+        $this->first_name = $args['first_name'] ?? '';
+        $this->last_name = $args['last_name'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->username = $args['username'] ?? '';
+        $this->hashed_password = $args['hashed_password'] ?? '';
+        
+    }
+}
 
 
 
--- public/staff/admins/index.php
+++ public/staff/admins/index.php
@@ -3,48 +3,42 @@
 <?php
   
 // Find all bicycles;
-$bicycles = Bicycle::find_all();
+$admins = Admin::find_all();
   
 ?>
-<?php $page_title = 'Bicycles'; ?>
+<?php $page_title = 'Admins'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
   <div class="bicycles listing">
-    <h1>Bicycles</h1>
+    <h1>Admins</h1>
 
     <div class="actions">
-      <a class="action" href="<?php echo url_for('/staff/bicycles/new.php'); ?>">Add Bicycle</a>
+      <a class="action" href="<?php echo url_for('/staff/admins/new.php'); ?>">Add Admin</a>
     </div>
 
   	<table class="list">
       <tr>
         <th>ID</th>
-        <th>Brand</th>
-        <th>Model</th>
-        <th>Year</th>
-        <th>Category</th>
-        <th>Gender</th>
-        <th>Color</th>
-        <th>Price</th>
+        <th>first_name</th>
+        <th>last_name</th>
+        <th>email</th>
+        <th>username</th>
         <th>&nbsp;</th>
         <th>&nbsp;</th>
         <th>&nbsp;</th>
       </tr>
 
-      <?php foreach($bicycles as $bicycle) { ?>
+      <?php foreach($admins as $admin) { ?>
         <tr>
-          <td><?php echo h($bicycle->id); ?></td>
-          <td><?php echo h($bicycle->brand); ?></td>
-          <td><?php echo h($bicycle->model); ?></td>
-          <td><?php echo h($bicycle->year); ?></td>
-          <td><?php echo h($bicycle->category); ?></td>
-          <td><?php echo h($bicycle->gender); ?></td>
-          <td><?php echo h($bicycle->color); ?></td>
-          <td><?php echo h($bicycle->price); ?></td>
-          <td><a class="action" href="<?php echo url_for('/staff/bicycles/show.php?id=' . h(u($bicycle->id))); ?>">View</a></td>
-          <td><a class="action" href="<?php echo url_for('/staff/bicycles/edit.php?id=' . h(u($bicycle->id))); ?>">Edit</a></td>
-          <td><a class="action" href="<?php echo url_for('/staff/bicycles/delete.php?id=' . h(u($bicycle->id))); ?>">Delete</a></td>
+          <td><?php echo h($admin->id); ?></td>
+          <td><?php echo h($admin->first_name); ?></td>
+          <td><?php echo h($admin->last_name); ?></td>
+          <td><?php echo h($admin->email); ?></td>
+          <td><?php echo h($admin->username); ?></td>
+          <td><a class="action" href="<?php echo url_for('/staff/admins/show.php?id=' . h(u($admin->id))); ?>">View</a></td>
+          <td><a class="action" href="<?php echo url_for('/staff/admins/edit.php?id=' . h(u($admin->id))); ?>">Edit</a></td>
+          <td><a class="action" href="<?php echo url_for('/staff/admins/delete.php?id=' . h(u($admin->id))); ?>">Delete</a></td>
     	  </tr>
       <?php } ?>
   	</table>
