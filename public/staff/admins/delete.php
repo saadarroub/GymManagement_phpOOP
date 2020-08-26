52bd5de5ef127c87df6550336522bd131d0da759
SAAD ARROUB <30563109+saadarroub@users.noreply.github.com>
2020-08-26 20:36:28 +0100

    msg

--- public/staff/admins/delete.php
+++ public/staff/admins/delete.php
@@ -3,20 +3,20 @@
 require_once('../../../private/initialize.php');
 
 if(!isset($_GET['id'])) {
-  redirect_to(url_for('/staff/bicycles/index.php'));
+  redirect_to(url_for('/staff/admins/index.php'));
 }
 $id = $_GET['id'];
-$bicycle = Bicycle::find_by_id($id);
-if($bicycle == false){
-  redirect_to(url_for('staff/bicycles/index.php'));
+$admin = Admin::find_by_id($id);
+if($admin == false){
+  redirect_to(url_for('staff/admins/index.php'));
 }
 
 if(is_post_request()) {
 
-  // Delete bicycle
-  $result = $bicycle->delete();
-  $_SESSION['message'] = 'The bicycle was deleted successfully.';
-  redirect_to(url_for('/staff/bicycles/index.php'));
+  // Delete admin
+  $result = $admin->delete();
+  $_SESSION['message'] = 'The admin was deleted successfully.';
+  redirect_to(url_for('/staff/admins/index.php'));
   
  
 } else {
@@ -25,21 +25,21 @@ if(is_post_request()) {
 
 ?>
 
-<?php $page_title = 'Delete Bicycle'; ?>
+<?php $page_title = 'Delete admin'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/bicycles/index.php'); ?>">&laquo; Back to List</a>
+  <a class="back-link" href="<?php echo url_for('/staff/admins/index.php'); ?>">&laquo; Back to List</a>
 
-  <div class="bicycle delete">
-    <h1>Delete Bicycle</h1>
-    <p>Are you sure you want to delete this bicycle?</p>
-    <p class="item"><?php echo h($bicycle->brand); ?></p>
+  <div class="admin delete">
+    <h1>Delete admin</h1>
+    <p>Are you sure you want to delete this admin?</p>
+    <p class="item"><?php echo h($admin->first_name); ?></p>
 
-    <form action="<?php echo url_for('/staff/bicycles/delete.php?id=' . h(u($id))); ?>" method="post">
+    <form action="<?php echo url_for('/staff/admins/delete.php?id=' . h(u($id))); ?>" method="post">
       <div id="operations">
-        <input type="submit" name="commit" value="Delete Bicycle" />
+        <input type="submit" name="commit" value="Delete admin" />
       </div>
     </form>
   </div>
--- public/staff/admins/edit.php
+++ public/staff/admins/edit.php
@@ -6,21 +6,21 @@ if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/bicycles/index.php'));
 }
 $id = $_GET['id'];
-$bicycle = Bicycle::find_by_id($id);
-if($bicycle == false) {
+$admin = Admin::find_by_id($id);
+if($admin == false) {
   redirect_to(url_for('/staff/bicycles/index.php'));
 }
 
 if(is_post_request()) {
 
   // Save record using post parameters
-  $args = $_POST['bicycle'];
-  $bicycle->merge_attributes($args);
-  $result = $bicycle->save();
+  $args = $_POST['admin'];
+  $admin->merge_attributes($args);
+  $result = $admin->save();
 
   if($result === true) {
-    $_SESSION['message'] = 'The bicycle was updated successfully.';
-    redirect_to(url_for('/staff/bicycles/show.php?id=' . $id));
+    $_SESSION['message'] = 'The admin was updated successfully.';
+    redirect_to(url_for('/staff/admins/show.php?id=' . $id));
   } else {
     // show errors
   }
@@ -32,24 +32,24 @@ if(is_post_request()) {
 
 ?>
 
-<?php $page_title = 'Edit Bicycle'; ?>
+<?php $page_title = 'Edit admin'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/bicycles/index.php'); ?>">&laquo; Back to List</a>
+  <a class="back-link" href="<?php echo url_for('/staff/admins/index.php'); ?>">&laquo; Back to List</a>
 
-  <div class="bicycle edit">
-    <h1>Edit Bicycle</h1>
+  <div class="admin edit">
+    <h1>Edit admin</h1>
 
-    <?php  echo display_errors($bicycle->errors); ?>
+    <?php  echo display_errors($admin->errors); ?>
 
-    <form action="<?php echo url_for('/staff/bicycles/edit.php?id=' . h(u($id))); ?>" method="post">
+    <form action="<?php echo url_for('/staff/admins/edit.php?id=' . h(u($id))); ?>" method="post">
 
       <?php include('form_fields.php'); ?>
       
       <div id="operations">
-        <input type="submit" value="Edit Bicycle" />
+        <input type="submit" value="Edit admin" />
       </div>
     </form>
 
